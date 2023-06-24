dd44c6c0b0ba6f034abb6ba9d4366f2b4a42c51f
Jordan Sarkodie <jsarkodie@jsarkod-ltmvcgr.internal.salesforce.com>
2023-06-23 17:06:34 -0700

    added finals

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,39 @@
 // YOUR CODE HERE
+const express = require('express')
+const cors = require('cors')
+const db = require('./data/db.json')
+const fs = require('fs')
+
+const app = express()
+
+
+
+
+
+
+app.use(cors())
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send(db)
+})
+
+
+app.get('/:id', (req, res) => {
+    const {id} = req.params
+    let productsList = JSON.parse(fs.readFileSync('./data/db.json'));
+    if(!productsList.products[id]) {
+        res.status(404).send({ message: "Item is not found"});
+        return;
+    }
+     else {
+        res.status(200).send(productsList.products[id])
+     }
+} )
+
+
+module.exports = app;
